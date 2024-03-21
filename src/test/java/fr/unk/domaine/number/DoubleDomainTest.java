2df2f5788b75a6434fd1c30ad0a06569a1f8b3b2
Fredoch02fr <145009505+Fredoch02fr@users.noreply.github.com>
2024-03-21 12:07:38 +0100

    Update DoubleDomainTest.java

--- src/test/java/fr/unk/domaine/number/DoubleDomainTest.java
+++ src/test/java/fr/unk/domaine/number/DoubleDomainTest.java
@@ -1,37 +1,61 @@
 package fr.unk.domaine.number;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 import java.util.Random;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class DoubleDomainTest {
+    Random random = new Random();
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        System.out.println ("Avant toutes les executions");
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+        System.out.println ("Apres toutes les executions");
+    }
+
+    @BeforeEach
+    void setUp() throws Exception {
+        System.out.println ("avant une fonction sous test");
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        System.out.println ("apres une fonction sous test");
+    }
     @Test
-    void testgetPossibility() {
-        Random random = new Random();
-        double double1;
-        double double2;
-        double double3 = 0.0;
-        double double4 = 100.0;
-        double double5;
+    void testFunctionalGetPossibility() {
+        //génération aléatoire
+        double val1;
+        double val2;
+        double val3 = 0.0;
+        double val4 = 100.0;
+        double val5;
         do {
-            double1 = random.nextInt();
-        } while (double1 < double3);
+            val1 = random.nextInt();
+        } while (val1 < val3);
         do {
-            double2 = random.nextInt();
-        } while (double2 > double4);
+            val2 = random.nextInt();
+        } while (val2 > val4);
         do {
-            double5 = random.nextInt();
-        } while (double5 > double4 || double5 < double3);
-        DoubleDomain doubleDomain = new DoubleDomain(double3, double4, 1.0);
+            val5 = random.nextInt();
+        } while (val5 > val4 || val5 < val3);
+        //init 5 valeurs , une valeur double5 compris dans un intervalle double3 et double4 ainsi qu'une valeur supérieur double2 et une valeur inférieur double1 le tout aléatoirement
+        DoubleDomain doubleDomain = new DoubleDomain(val3,val4,1.0);
         List<Double> list = doubleDomain.getPossibility();
-        assertEquals(((double4 - double3)) + 1, list.size());
-        assertFalse(list.contains(double1));
-        assertFalse(list.contains(double2));
-        assertTrue(list.contains(double3));
-        assertTrue(list.contains(double4));
-        assertTrue(list.contains(double5));
+        //Test si cela créer bien le bon nombre de possibilités dans le domaine
+        assertEquals(((val4-val3)/1.0)+1, list.size());
+        //test si double1 et double2 sont bien exclu du domaine
+        assertFalse(list.contains(val1));
+        assertFalse(list.contains(val2));
+        //test si double3,4,5 sont bien compris dans le domaine
+        assertTrue(list.contains(val3));
+        assertTrue(list.contains(val4));
+        assertTrue(list.contains(val5));
     }
 }
