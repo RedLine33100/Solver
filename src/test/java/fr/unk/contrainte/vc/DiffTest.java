97ec83899bdab879583b25ef5348d6098c7a82ec
Fredoch02fr <145009505+Fredoch02fr@users.noreply.github.com>
2024-03-21 11:59:45 +0100

    Update DiffTest.java

--- src/test/java/fr/unk/contrainte/vc/DiffTest.java
+++ src/test/java/fr/unk/contrainte/vc/DiffTest.java
@@ -1,77 +1,125 @@
 package fr.unk.contrainte.vc;
 
-import fr.unk.variable.Getter;
 import org.junit.jupiter.api.Test;
+import fr.unk.variable.VarGetter;
+import org.junit.jupiter.api.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class DiffTest {
+    Random random = new Random();
+    Map<String,Object> objectMap = new HashMap<>();
 
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        System.out.println ("Avant toutes les executions");
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+        System.out.println ("Apres toutes les executions");
+    }
+
+    @BeforeEach
+    void setUp() throws Exception {
+        System.out.println ("avant une fonction sous test");
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        System.out.println ("apres une fonction sous test");
+    }
     @Test
-    void testsatisfied() {
-        Random random = new Random();
-        int int1 = random.nextInt();
-        int int2;
+    void testFonctionnelSatisfiedInt() {
+        int val1 = random.nextInt();
+        int val2;
         do {
-            int2 = random.nextInt();
-        } while (int2 == int1);
-        Getter<Integer> v0_int = new Getter<>(int1);
-        Getter<Integer> v1_int = new Getter<>(int2);
-        Getter<Integer> v2_int = new Getter<>(int1);
-        Diff<Integer> diff_2_var_var = new Diff<>(v1_int, v0_int);
-        Diff<Integer> diff_1_var_var = new Diff<>(v2_int, v0_int);
-        Diff<Integer> diff_1_var_int = new Diff<>(v0_int, int1);
-        Diff<Integer> diff_1_int_var = new Diff<>(int1, v1_int);
-        assertTrue(diff_2_var_var.satisfied());
-        assertFalse(diff_1_var_var.satisfied());
-        assertTrue(diff_1_int_var.satisfied());
-        assertFalse(diff_1_var_int.satisfied());
+            val2 = random.nextInt();
+        } while (val2 == val1);
+        //init 3 variables ( var2 == var0 != var1) pour voir si cela marche avec 2 variables différentes de même valeur:
+        VarGetter var0 = new VarGetter(val1);
+        VarGetter var1 = new VarGetter(val2);
+        VarGetter var2 = new VarGetter(val1);
+        //test si Diff(var1,var0) = true:
+        Diff isVarDiff = new Diff(var1,var0);
+        assertTrue(isVarDiff.satisfied(objectMap));
+        //test si Diff(var2,var0) = false:
+        Diff isNotVarDiff = new Diff(var2,var0);
+        assertFalse(isNotVarDiff.satisfied(objectMap));
+        //objectif tester si la fonction Diff fonctionne avec une variable et une valeur de type int
+        //test si Diff(valeur int = var0 ,var0) = false:
+        Diff isVarAndValDiff = new Diff(var0,val1);
+        assertFalse(isVarAndValDiff.satisfied(objectMap));
+        //test si Diff(valeur int = var1 ,var0) = true:
+        Diff isNotValAndVarDiff = new Diff(val1,var1);
+        assertTrue(isNotValAndVarDiff.satisfied(objectMap));
 
-        float float1 = random.nextFloat();
-        float float2;
+    }
+    @Test
+    void testFonctionnelSatisfiedFloat() {
+        float val1 = random.nextFloat();
+        float val2;
         do {
-            float2 = random.nextFloat();
-        } while (float2 == float1);
-        Getter<Float> v0_float = new Getter<>(float1);
-        Getter<Float> v1_float = new Getter<>(float2);
-        Getter<Float> v2_float = new Getter<>(float1);
-        Diff<Float> diff_2_var_varf = new Diff<>(v1_float, v0_float);
-        Diff<Float> diff_1_var_varf = new Diff<>(v2_float, v0_float);
-        Diff<Float> diff_1_var_float = new Diff<>(v0_float, float1);
-        Diff<Float> diff_1_float_var = new Diff<>(float1, v1_float);
-        assertTrue(diff_2_var_varf.satisfied());
-        assertFalse(diff_1_var_varf.satisfied());
-        assertTrue(diff_1_float_var.satisfied());
-        assertFalse(diff_1_var_float.satisfied());
-
-        double double1 = random.nextDouble();
-        double double2;
+            val2 = random.nextFloat();
+        } while (val2 == val1);
+        //init 3 variables ( var2 == var0 != var1) pour voir si cela marche avec 2 variables différentes de même valeur:
+        VarGetter var0 = new VarGetter(val1);
+        VarGetter var1 = new VarGetter(val2);
+        VarGetter var2 = new VarGetter(val1);
+        //test si Diff(var,var0) = true:
+        Diff isVarDiff = new Diff(var1,var0);
+        assertTrue(isVarDiff.satisfied(objectMap));
+        //test si Diff(var2,var0) = false:
+        Diff isNotVarDiff = new Diff(var2,var0);
+        assertFalse(isNotVarDiff.satisfied(objectMap));
+        //objectif tester si la fonction Diff fonctionne avec une variable et une valeur de type float
+        //test si Diff(valeur float = var0 ,var0) = false:
+        Diff isVarAndValDiff = new Diff(var0,val1);
+        assertFalse(isVarAndValDiff.satisfied(objectMap));
+        //test si Diff(valeur float = var1 ,var0) = true:
+        Diff isNotValAndVarDiff = new Diff(val1,var1);
+        assertTrue(isNotValAndVarDiff.satisfied(objectMap));
+    }
+    @Test
+    void testFonctionnelSatisfiedDouble() {
+        double val1 = random.nextDouble();
+        double val2;
         do {
-            double2 = random.nextDouble();
-        } while (double2 == double1);
-        Getter<Double> v0_double = new Getter<>(double1);
-        Getter<Double> v1_double = new Getter<>(double2);
-        Getter<Double> v2_double = new Getter<>(double1);
-        Diff<Double> diff_2_var_vard = new Diff<>(v1_double, v0_double);
-        Diff<Double> diff_1_var_vard = new Diff<>(v2_double, v0_double);
-        Diff<Double> diff_1_var_double = new Diff<>(v0_double, double1);
-        Diff<Double> diff_1_double_var = new Diff<>(double1, v1_double);
-        assertTrue(diff_2_var_vard.satisfied());
-        assertFalse(diff_1_var_vard.satisfied());
-        assertTrue(diff_1_double_var.satisfied());
-        assertFalse(diff_1_var_double.satisfied());
+            val2 = random.nextDouble();
+        } while (val2 == val1);
+        //init 3 variables ( var2 == var0 != var1) pour voir si cela marche avec 2 variables différentes de même valeur:
+        VarGetter var0 = new VarGetter(val1);
+        VarGetter var1 = new VarGetter(val2);
+        VarGetter var2 = new VarGetter(val1);
+        //test si Diff(var1,vra0) = true:
+        Diff isVarDiff = new Diff(var1,var0);
+        assertTrue(isVarDiff.satisfied(objectMap));
+        //test si Diff(var2,var0) = false:
+        Diff isNotVarDiff = new Diff(var2,var0);
+        assertFalse(isNotVarDiff.satisfied(objectMap));
+        //objectif tester si la fonction Diff fonctionne avec une variable et une valeur de type double
+        //test si Diff(valeur double = var0 ,var0) = false:
+        Diff isVarAndValDiff = new Diff(var0,val1);
+        assertFalse(isVarAndValDiff.satisfied(objectMap));
+        //test si Diff(valeur double = var1 ,var0) = true:
+        Diff isNotValAndVarDiff = new Diff(val1,var1);
+        assertTrue(isNotValAndVarDiff.satisfied(objectMap));
 
-        //*A modifier voir avec le groupe*//
-        //erreur donc il faut rajouter une condition.
-        /*VarGetter v3_int = new VarGetter(10);
-        VarGetter v3_double = new VarGetter(10.0);
-        VarGetter v3_float = new VarGetter(10.0f);
-        Diff diff_type_var = new Diff(v3_int,v3_double);
-        Diff diff_type = new Diff(5,10.0f);
-        assertFalse(diff_type.satisfied(objectMap));
+
+    }
+    @Test
+    void testSatisfied() {
+        //ne fonctionne pas quand il y a des types différent comme avec Inf
+        /*VarGetter vInt = new VarGetter(10);
+        VarGetter vDouble = new VarGetter(10.0);
+        VarGetter vFloat = new VarGetter(10.0f);
+        Diff isNotIntDouble = new Diff(vInt,vDouble);
+        assertFalse(isVar.satisfied(objectMap));
+        Diff isNotIntDouble = new Diff(5,10.0f);
+        assertFalse(int_dif_float_true.satisfied(objectMap));
         */
     }
 }
