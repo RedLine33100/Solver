4966038e0072c6c09a3dfc8831512062eab0f8df
Fredoch02fr <145009505+Fredoch02fr@users.noreply.github.com>
2024-03-21 12:10:17 +0100

    Update IntDomainTest.java

--- src/test/java/fr/unk/domaine/number/IntDomainTest.java
+++ src/test/java/fr/unk/domaine/number/IntDomainTest.java
@@ -2,35 +2,36 @@ package fr.unk.domaine.number;
 
 import org.junit.jupiter.api.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class IntDomainTest {
-
+    Random random = new Random();
     @BeforeAll
-    static void setUpBeforeClass() {
-        System.out.println("Avant toutes les executions");
+    static void setUpBeforeClass() throws Exception {
+        System.out.println ("Avant toutes les executions");
     }
 
     @AfterAll
-    static void tearDownAfterClass() {
-        System.out.println("Apres toutes les executions");
+    static void tearDownAfterClass() throws Exception {
+        System.out.println ("Apres toutes les executions");
     }
 
     @BeforeEach
-    void setUp() {
-        System.out.println("avant une fonction sous test");
+    void setUp() throws Exception {
+        System.out.println ("avant une fonction sous test");
     }
 
     @AfterEach
-    void tearDown() {
-        System.out.println("apres une fonction sous test");
+    void tearDown() throws Exception {
+        System.out.println ("apres une fonction sous test");
     }
 
     @Test
-    void testGetPossibilityStructurel() {
+    void testStructurelGetPossibility() {
         // Cas standard : valeurs positives avec un pas positif
         IntDomain domain1 = new IntDomain(0, 10, 2);
         List<Integer> possibilities1 = domain1.getPossibility();
@@ -59,7 +60,7 @@ class IntDomainTest {
         assertEquals(-4, possibilities3.get(3));
         assertEquals(-2, possibilities3.get(4));
         */
-
+        /*
         // Cas où les valeurs minimale, maximale et le pas sont négatifs : valeurs négatives avec un pas positif
         IntDomain domain4 = new IntDomain(-10, -2, 2);
         List<Integer> possibilities4 = domain4.getPossibility();
@@ -68,34 +69,40 @@ class IntDomainTest {
         // Cas où le pas est zéro : devrait renvoyer une liste vide
         IntDomain domain5 = new IntDomain(0, 10, 0);
         List<Integer> possibilities5 = domain5.getPossibility();
-        assertTrue(possibilities5.isEmpty());
+        assertNull(possibilities5);
+        */
+
     }
 
 
     @Test
-    void testGetPossibilityFonctionel() {
-        Random random = new Random();
-        int int1;
-        int int2;
-        int int3 = 0;
-        int int4 = 100;
-        int int5;
+    void testFunctionalGetPossibility() {
+        //génération aléatoire
+        int val1;
+        int val2;
+        int val3 = 0;
+        int val4 = 100;
+        int val5;
         do {
-            int1 = random.nextInt();
-        } while (int1 < int3);
+            val1 = random.nextInt();
+        } while (val1 < val3);
         do {
-            int2 = random.nextInt();
-        } while (int2 > int4);
+            val2 = random.nextInt();
+        } while (val2 > val4);
         do {
-            int5 = random.nextInt();
-        } while (int5 < int3 || int5 > int4);
-        IntDomain intDomain = new IntDomain(int3, int4, 1);
+            val5 = random.nextInt();
+        } while (val5 < val3 || val5 > val4);
+        //init 5 valeurs , une valeur int5 compris dans un intervalle int3 et int4 ainsi qu'une valeur supérieur int2 et une valeur inférieur int1 le tout aléatoirement
+        IntDomain intDomain = new IntDomain(val3,val4,1);
         List<Integer> list = intDomain.getPossibility();
-        assertEquals(((int4 - int3)) + 1, list.size());
-        assertFalse(list.contains(int1));
-        assertFalse(list.contains(int2));
-        assertTrue(list.contains(int3));
-        assertTrue(list.contains(int4));
-        assertTrue(list.contains(int5));
+        //Test si cela créer bien le bon nombre de possibilités dans le domaine
+        assertEquals(((val4-val3)/1)+1, list.size());
+        //test si int1 et int2 sont bien exclu du domaine
+        assertFalse(list.contains(val1));
+        assertFalse(list.contains(val2));
+        //test si int3,4,5 sont bien compris dans le domaine
+        assertTrue(list.contains(val3));
+        assertTrue(list.contains(val4));
+        assertTrue(list.contains(val5));
     }
 }
