604bc7a8c7d50337de9c77c39c5bcab361fc9c4e
Fredoch02fr <145009505+Fredoch02fr@users.noreply.github.com>
2024-03-21 12:05:47 +0100

    Update SupTest.java

--- src/test/java/fr/unk/contrainte/vc/SupTest.java
+++ src/test/java/fr/unk/contrainte/vc/SupTest.java
@@ -1,65 +1,109 @@
 package fr.unk.contrainte.vc;
 
-import fr.unk.variable.Getter;
 import org.junit.jupiter.api.Test;
+import fr.unk.variable.VarGetter;
+import org.junit.jupiter.api.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SupTest {
+    Random random = new Random();
+    Map<String,Object> objectMap = new HashMap<>();
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        System.out.println ("Avant toutes les executions");
+    }
 
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+        System.out.println ("Apres toutes les executions");
+    }
+
+    @BeforeEach
+    void setUp() throws Exception {
+        System.out.println ("avant une fonction sous test");
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        System.out.println ("apres une fonction sous test");
+    }
     @Test
-    void testsatisfied() {
-        Random random = new Random();
-        int int1 = random.nextInt();
-        int int2;
+    void testFunctionalSatisfiedFloat() {
+        float val1 = random.nextFloat();
+        float val2;
         do {
-            int2 = random.nextInt();
-        } while (int2 < int1);
-        Getter<Integer> v0_int = new Getter<>(int1);
-        Getter<Integer> v1_int = new Getter<>(int2);
-        Sup<Integer> sup_2_var_var = new Sup<>(v1_int, v0_int, false);
-        Sup<Integer> sup_1_var_var = new Sup<>(v0_int, v1_int, false);
-        Sup<Integer> sup_1_var_int = new Sup<>(v1_int, int1, false);
-        Sup<Integer> sup_1_int_var = new Sup<>(int1, v1_int, false);
-        assertTrue(sup_2_var_var.satisfied());
-        assertFalse(sup_1_var_var.satisfied());
-        assertFalse(sup_1_int_var.satisfied());
-        assertTrue(sup_1_var_int.satisfied());
-
-        float float1 = random.nextFloat();
-        float float2;
+            val2 = random.nextFloat();
+        } while (val2 < val1);
+        //init 2 variable (v0 < v1):
+        VarGetter var0 = new VarGetter(val1);
+        VarGetter var1 = new VarGetter(val2);
+        //test si Sup(var1,var0)= true:
+        Sup isVarSup = new Sup(var1,var0,false);
+        assertTrue(isVarSup.satisfied(objectMap));
+        //test si Sup(var0,var1)= false:
+        Sup isNotVarSup = new Sup(var0,var1,false);
+        assertFalse(isNotVarSup.satisfied(objectMap));
+        //objectif tester si la fonction Sup fonctionne avec une variable et une valeur de type double
+        //test si Sup(var1, une valeur float = var0)= true:
+        Sup isVarAndValSup = new Sup(var1,val1,false);
+        assertTrue(isVarAndValSup.satisfied(objectMap));
+        //test si Inf(une valeur float = var0,var1)= false:
+        Sup isNotValAndVarSup = new Sup(val1,var1,false);
+        assertFalse(isNotValAndVarSup.satisfied(objectMap));
+    }
+    @Test
+    void testFunctionalSatisfiedDouble() {
+        double val1 = random.nextDouble();
+        double val2;
         do {
-            float2 = random.nextFloat();
-        } while (float2 < float1);
-        Getter<Float> v0_float = new Getter<>(float1);
-        Getter<Float> v1_float = new Getter<>(float2);
-        Sup<Float> sup_2_var_varf = new Sup<>(v1_float, v0_float, false);
-        Sup<Float> sup_1_var_varf = new Sup<>(v0_float, v1_float, false);
-        Sup<Float> sup_1_var_float = new Sup<>(v1_float, float1, false);
-        Sup<Float> sup_1_float_var = new Sup<>(float1, v1_float, false);
-        assertTrue(sup_2_var_varf.satisfied());
-        assertFalse(sup_1_var_varf.satisfied());
-        assertFalse(sup_1_float_var.satisfied());
-        assertTrue(sup_1_var_float.satisfied());
-
-        double double1 = random.nextDouble();
-        double double2;
-        do {
-            double2 = random.nextDouble();
-        } while (double2 < double1);
-        Getter<Double> v0_double = new Getter<>(double1);
-        Getter<Double> v1_double = new Getter<>(double2);
-        Sup<Double> sup_2_var_vard = new Sup<>(v1_double, v0_double, false);
-        Sup<Double> sup_1_var_vard = new Sup<>(v0_double, v1_double, false);
-        Sup<Double> sup_1_var_double = new Sup<>(v1_double, double1, false);
-        Sup<Double> sup_1_double_var = new Sup<>(double1, v1_double, false);
-        assertTrue(sup_2_var_vard.satisfied());
-        assertFalse(sup_1_var_vard.satisfied());
-        assertFalse(sup_1_double_var.satisfied());
-        assertTrue(sup_1_var_double.satisfied());
+            val2 = random.nextDouble();
+        } while (val2 < val1);
+        //init 2 variable (v0 < v1):
+        VarGetter var0 = new VarGetter(val1);
+        VarGetter var1 = new VarGetter(val2);
+        //test si Sup(var1,var0)= true:
+        Sup isVarSup = new Sup(var1,var0,false);
+        assertTrue(isVarSup.satisfied(objectMap));
+        //test si Sup(var0,var1)= false:
+        Sup isNotVarSup = new Sup(var0,var1,false);
+        assertFalse(isNotVarSup.satisfied(objectMap));
+        //objectif tester si la fonction Sup fonctionne avec une variable et une valeur de type double
+        //test si Sup(var1, une valeur double = var0)= true:
+        Sup isVarAndValSup = new Sup(var1,val1,false);
+        assertTrue(isVarAndValSup.satisfied(objectMap));
+        //test si Inf(une valeur double = var0,var1)= false:
+        Sup isNotValAndVarSup = new Sup(val1,var1,false);
+        assertFalse(isNotValAndVarSup.satisfied(objectMap));
 
     }
+
+    @Test
+    void testFunctionalSatisfiedInt() {
+        int val1 = random.nextInt();
+        int val2;
+        do {
+            val2 = random.nextInt();
+        } while (val2 < val1);
+        //init 2 variable (v0 < v1):
+        VarGetter var0 = new VarGetter(val1);
+        VarGetter var1 = new VarGetter(val2);
+        //test si Sup(var1,var0)= true:
+        Sup isVarSup = new Sup(var1,var0,false);
+        assertTrue(isVarSup.satisfied(objectMap));
+        //test si Sup(var0,var1)= false:
+        Sup isNotVarSup = new Sup(var0,var1,false);
+        assertFalse(isNotVarSup.satisfied(objectMap));
+        //objectif tester si la fonction Sup fonctionne avec une variable et une valeur de type int
+        //test si Sup(var1, une valeur int = var0)= true:
+        Sup isVarAndValSup = new Sup(var1,val1,false);
+        assertTrue(isVarAndValSup.satisfied(objectMap));
+        //test si Sup(var1, une valeur int = var0)= true:
+        Sup isNotValAndVarSup = new Sup(val1,var1,false);
+        assertFalse(isNotValAndVarSup.satisfied(objectMap));
+    }
+    //pareil que pour Inf, il ne peut pas y avoir différent type.
 }
